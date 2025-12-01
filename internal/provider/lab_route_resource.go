f6ac4cf08d9743da67273361365bbcfc4c9935a0
Colin <cwelch@masslight.com>
2025-12-01 07:14:05 -0500

    feat(lab): Add optional route attributes (#63)
    * feat(lab): Add optional route attributes
    
    * tweak(lab): Add new `primary_phone` attribute
    
    * fix(lab): Functional optional parameters

--- internal/provider/lab_route_resource.go
+++ internal/provider/lab_route_resource.go
@@ -2,41 +2,117 @@ package provider
 
 import (
 	"context"
+	"regexp"
 	"strings"
 
+	"github.com/hashicorp/terraform-plugin-framework/attr"
 	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/resource/identityschema"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/objectplanmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hashicorp/terraform-plugin-framework/types/basetypes"
 	"github.com/masslight/terraform-provider-oystehr/internal/client"
 )
 
+var (
+	phoneRegexp = regexp.MustCompile(`^\(\d{3}\) \d{3}-\d{4}$`) // e.g., (123) 456-7890
+)
+
+type LabRouteAddress struct {
+	Address1          types.String `tfsdk:"address1"`
+	Address2          types.String `tfsdk:"address2"`
+	City              types.String `tfsdk:"city"`
+	StateProvinceCode types.String `tfsdk:"state_province_code"`
+	PostalCode        types.String `tfsdk:"postal_code"`
+}
+
 type LabRoute struct {
-	ID            types.String `tfsdk:"id"`
-	LabID         types.String `tfsdk:"lab_id"`
-	AccountNumber types.String `tfsdk:"account_number"`
+	ID                        types.String `tfsdk:"id"`
+	AccountNumber             types.String `tfsdk:"account_number"`
+	LabID                     types.String `tfsdk:"lab_id"`
+	LabName                   types.String `tfsdk:"lab_name"`
+	PrimaryID                 types.String `tfsdk:"primary_id"`
+	PrimaryName               types.String `tfsdk:"primary_name"`
+	PrimaryAddress            types.Object `tfsdk:"primary_address"`
+	PrimaryPhone              types.String `tfsdk:"primary_phone"`
+	ClientSiteID              types.String `tfsdk:"client_site_id"`
+	EULAVersion               types.String `tfsdk:"eula_version"`
+	EULAAccepterFullName      types.String `tfsdk:"eula_accepter_full_name"`
+	EULAAcceptanceDateTimeUTC types.String `tfsdk:"eula_acceptance_date_time_utc"`
 }
 
 type LabRouteIdentityModel struct {
 	ID types.String `tfsdk:"id"`
 }
 
+var labRouteAddressAttributeTypes map[string]attr.Type = map[string]attr.Type{
+	"address1":            types.StringType,
+	"address2":            types.StringType,
+	"city":                types.StringType,
+	"state_province_code": types.StringType,
+	"postal_code":         types.StringType,
+}
+
 func convertLabRouteToClientLabRoute(ctx context.Context, labRoute LabRoute) client.LabRoute {
+	var primaryAddress *client.LabRouteAddress
+	if !labRoute.PrimaryAddress.IsNull() {
+		var tfAddress LabRouteAddress
+		labRoute.PrimaryAddress.As(ctx, &tfAddress, basetypes.ObjectAsOptions{UnhandledNullAsEmpty: true, UnhandledUnknownAsEmpty: true})
+		primaryAddress = &client.LabRouteAddress{
+			Address1:          tfStringToStringPointer(tfAddress.Address1),
+			Address2:          tfStringToStringPointer(tfAddress.Address2),
+			City:              tfStringToStringPointer(tfAddress.City),
+			StateProvinceCode: tfStringToStringPointer(tfAddress.StateProvinceCode),
+			PostalCode:        tfStringToStringPointer(tfAddress.PostalCode),
+		}
+	}
 	return client.LabRoute{
-		RouteGUID:     tfStringToStringPointer(labRoute.ID),
-		LabGUID:       tfStringToStringPointer(labRoute.LabID),
-		AccountNumber: tfStringToStringPointer(labRoute.AccountNumber),
+		RouteGUID:                 tfStringToStringPointer(labRoute.ID),
+		AccountNumber:             tfStringToStringPointer(labRoute.AccountNumber),
+		LabGUID:                   tfStringToStringPointer(labRoute.LabID),
+		LabName:                   tfStringToStringPointer(labRoute.LabName),
+		PrimaryID:                 tfStringToStringPointer(labRoute.PrimaryID),
+		PrimaryName:               tfStringToStringPointer(labRoute.PrimaryName),
+		PrimaryAddress:            primaryAddress,
+		PrimaryPhone:              tfStringToStringPointer(labRoute.PrimaryPhone),
+		ClientSiteID:              tfStringToStringPointer(labRoute.ClientSiteID),
+		EULAVersion:               tfStringToStringPointer(labRoute.EULAVersion),
+		EULAAccepterFullName:      tfStringToStringPointer(labRoute.EULAAccepterFullName),
+		EULAAcceptanceDateTimeUTC: tfStringToStringPointer(labRoute.EULAAcceptanceDateTimeUTC),
 	}
 }
 
 func convertClientLabRouteToLabRoute(ctx context.Context, clientLabRoute *client.LabRoute) LabRoute {
+	var primaryAddress types.Object
+	if clientLabRoute.PrimaryAddress == nil {
+		primaryAddress = types.ObjectNull(labRouteAddressAttributeTypes)
+	} else {
+		tfAddress := LabRouteAddress{
+			Address1:          stringPointerToTfString(clientLabRoute.PrimaryAddress.Address1),
+			Address2:          stringPointerToTfString(clientLabRoute.PrimaryAddress.Address2),
+			City:              stringPointerToTfString(clientLabRoute.PrimaryAddress.City),
+			StateProvinceCode: stringPointerToTfString(clientLabRoute.PrimaryAddress.StateProvinceCode),
+			PostalCode:        stringPointerToTfString(clientLabRoute.PrimaryAddress.PostalCode),
+		}
+		primaryAddress, _ = types.ObjectValueFrom(ctx, labRouteAddressAttributeTypes, tfAddress)
+	}
 	return LabRoute{
-		ID:            stringPointerToTfString(clientLabRoute.RouteGUID),
-		LabID:         stringPointerToTfString(clientLabRoute.LabGUID),
-		AccountNumber: stringPointerToTfString(clientLabRoute.AccountNumber),
+		ID:                        stringPointerToTfString(clientLabRoute.RouteGUID),
+		AccountNumber:             stringPointerToTfString(clientLabRoute.AccountNumber),
+		LabID:                     stringPointerToTfString(clientLabRoute.LabGUID),
+		LabName:                   stringPointerToTfString(clientLabRoute.LabName),
+		PrimaryID:                 stringPointerToTfString(clientLabRoute.PrimaryID),
+		PrimaryName:               stringPointerToTfString(clientLabRoute.PrimaryName),
+		PrimaryAddress:            primaryAddress,
+		PrimaryPhone:              stringPointerToTfString(clientLabRoute.PrimaryPhone),
+		ClientSiteID:              stringPointerToTfString(clientLabRoute.ClientSiteID),
+		EULAVersion:               stringPointerToTfString(clientLabRoute.EULAVersion),
+		EULAAccepterFullName:      stringPointerToTfString(clientLabRoute.EULAAccepterFullName),
+		EULAAcceptanceDateTimeUTC: stringPointerToTfString(clientLabRoute.EULAAcceptanceDateTimeUTC),
 	}
 }
 
@@ -67,6 +143,13 @@ func (r *LabRouteResource) Schema(_ context.Context, req resource.SchemaRequest,
 					stringplanmodifier.UseStateForUnknown(),
 				},
 			},
+			"account_number": schema.StringAttribute{
+				Required:    true,
+				Description: "The account number associated with the lab route.",
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.RequiresReplace(),
+				},
+			},
 			"lab_id": schema.StringAttribute{
 				Required:    true,
 				Description: "The unique identifier for the lab.",
@@ -74,9 +157,81 @@ func (r *LabRouteResource) Schema(_ context.Context, req resource.SchemaRequest,
 					stringplanmodifier.RequiresReplace(),
 				},
 			},
-			"account_number": schema.StringAttribute{
-				Required:    true,
-				Description: "The account number associated with the lab route.",
+			"lab_name": schema.StringAttribute{
+				Computed:    true,
+				Description: "The name of the lab.",
+			},
+			"primary_id": schema.StringAttribute{
+				Computed:    true,
+				Description: "The primary identifier for the lab route.",
+			},
+			"primary_name": schema.StringAttribute{
+				Optional:    true,
+				Description: "The primary name for the lab route.",
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.RequiresReplace(),
+				},
+			},
+			"primary_address": schema.SingleNestedAttribute{
+				Optional:    true,
+				Description: "The primary address for the lab route.",
+				Attributes: map[string]schema.Attribute{
+					"address1": schema.StringAttribute{
+						Required:    true,
+						Description: "The first line of the address.",
+					},
+					"address2": schema.StringAttribute{
+						Optional:    true,
+						Description: "The second line of the address.",
+					},
+					"city": schema.StringAttribute{
+						Required:    true,
+						Description: "The city of the address.",
+					},
+					"state_province_code": schema.StringAttribute{
+						Required:    true,
+						Description: "The state or province code of the address.",
+					},
+					"postal_code": schema.StringAttribute{
+						Required:    true,
+						Description: "The postal code of the address.",
+					},
+				},
+				PlanModifiers: []planmodifier.Object{
+					objectplanmodifier.RequiresReplace(),
+				},
+			},
+			"primary_phone": schema.StringAttribute{
+				Optional:    true,
+				Description: "The primary phone number for the lab route. Format: (123) 456-7890",
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.RequiresReplace(),
+				},
+			},
+			"client_site_id": schema.StringAttribute{
+				Optional:    true,
+				Description: "The client site identifier.",
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.RequiresReplace(),
+				},
+			},
+			"eula_version": schema.StringAttribute{
+				Optional:    true,
+				Description: "The version of the End User License Agreement (EULA) accepted.",
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.RequiresReplace(),
+				},
+			},
+			"eula_accepter_full_name": schema.StringAttribute{
+				Optional:    true,
+				Description: "The full name of the person who accepted the EULA.",
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.RequiresReplace(),
+				},
+			},
+			"eula_acceptance_date_time_utc": schema.StringAttribute{
+				Optional:    true,
+				Description: "The UTC date and time when the EULA was accepted.",
 				PlanModifiers: []planmodifier.String{
 					stringplanmodifier.RequiresReplace(),
 				},
@@ -121,6 +276,15 @@ func (r *LabRouteResource) Create(ctx context.Context, req resource.CreateReques
 	}
 
 	clientLabRoute := convertLabRouteToClientLabRoute(ctx, plan)
+
+	// Validate
+	if clientLabRoute.PrimaryPhone != nil && !phoneRegexp.MatchString(*clientLabRoute.PrimaryPhone) {
+		resp.Diagnostics.AddError(
+			"Invalid Primary Phone",
+			"The primary phone number must be in the format (123) 456-7890.",
+		)
+		return
+	}
 
 	createdRoute, err := r.client.Lab.CreateLabRoute(ctx, &clientLabRoute)
 	if err != nil {
